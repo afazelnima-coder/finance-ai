ac08cb768c1c207318201d3ded962de5af4007b9
Nima Afazel <afazelnima@gmail.com>
2026-01-08 18:47:25 -0800

    Created goal planning agent

---
+++ agents/goal-agent.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv  
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain.messages import HumanMessage
+from tavily import TavilyClient
+
+load_dotenv()
+
+tavily_client = TavilyClient()
+
+@tool
+def searchFinance(query: str) -> str:
+    """Searches for financial information on the web."""
+    return tavily_client.search(
+        query=query,
+        include_domains=["investopedia.com"],
+        num_results=1,
+        region="us",
+        language="en"
+    )
+
+# Create the agent
+agent = create_agent(
+    "gpt-5-nano",
+    tools=[searchFinance],
+    system_prompt=""""
+    You are a helpful finance assistant specialized in assisting with setting and planning goals. 
+    Answer only financial concepts. 
+    Any unrelated questions should be politely declined and specify that they are outside the scope of financial advice.
+    """
+)
+
+if __name__ == "__main__":
+    # Example interaction
+    user_input = "I want to save for a house down payment. What are some good savings strategies?"
+    response = agent.invoke({"messages": [HumanMessage(content=user_input)]})
+
+    print(response["messages"][-1].content)
